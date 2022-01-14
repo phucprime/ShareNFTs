63843c04353c6204fdc9115c008bc06077a77414
Phuc Nguyen <40540902+phucprime@users.noreply.github.com>
2022-01-14 22:13:47 +0700

    Update Settings.js

--- App/Screens/Settings/Settings.js
+++ App/Screens/Settings/Settings.js
@@ -1,62 +1,47 @@
-import React, {useEffect} from 'react';
-import {
-  ActivityIndicator,
-  FlatList,
-  Image,
-  ImageBackground,
-  Platform,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import {widthPercentageToDP as wp} from '../../Components/ResponsiveLayout';
-import AntDesign from 'react-native-vector-icons/AntDesign';
+import React, { useEffect } from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { widthPercentageToDP as wp } from "../../Components/ResponsiveLayout";
+import AntDesign from "react-native-vector-icons/AntDesign";
 
-const Settings = props => {
-  const [size, setSize] = React.useState('');
+const Settings = (props) => {
+  const [size, setSize] = React.useState("");
 
   useEffect(() => {});
 
   return (
     <SafeAreaView>
-      {/*<StatusBar*/}
-      {/*    hidden={false}*/}
-      {/*    backgroundColor="transparent"*/}
-      {/*    translucent={true}*/}
-      {/*/>*/}
-      <View style={{marginTop: 60, height: '100%'}}>
+      <View style={{ marginTop: 60, height: "100%" }}>
         <View
           style={{
-            flexDirection: 'row',
+            flexDirection: "row",
             height: 40,
-            justifyContent: 'center',
-            alignItems: 'center',
+            justifyContent: "center",
+            alignItems: "center",
             borderTopLeftRadius: 30,
             borderTopRightRadius: 30,
-          }}>
+          }}
+        >
           <View
             style={{
-              width: wp('75%'),
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
+              width: wp("75%"),
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
             <Text
               style={{
                 fontSize: 22,
-                fontWeight: 'bold',
-                color: '#000',
-                alignSelf: 'center',
-              }}>
+                fontWeight: "bold",
+                color: "#000",
+                alignSelf: "center",
+              }}
+            >
               Settings
             </Text>
           </View>
 
-          <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
-            <AntDesign name={'close'} size={25} color={'#000'} />
+          <TouchableOpacity onPress={() => props.navigation.navigate("Home")}>
+            <AntDesign name={"close"} size={25} color={"#000"} />
           </TouchableOpacity>
         </View>
       </View>
