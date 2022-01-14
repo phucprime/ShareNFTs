cf5361a3ea06e470205bb103d5acd8e461d4bfe0
Phuc Nguyen <40540902+phucprime@users.noreply.github.com>
2022-01-14 22:12:48 +0700

    Update index.js

--- App/Components/Loader/index.js
+++ App/Components/Loader/index.js
@@ -1,8 +1,8 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import {BallIndicator} from 'react-native-indicators';
+import PropTypes from "prop-types";
+import React from "react";
+import { BallIndicator } from "react-native-indicators";
 
-const Loader = props => {
+const Loader = (props) => {
   return (
     <BallIndicator count={props.count} color={props.color} size={props.size} />
   );
@@ -16,19 +16,8 @@ Loader.propTypes = {
 
 Loader.defaultProps = {
   count: 5,
-  color: 'white',
+  color: "white",
   size: 20,
 };
 
 export default Loader;
-
-/*
-USAGE:
-
-  <Loader
-    count={5}
-    color="white"
-    size={}
-  />
-
-*/
