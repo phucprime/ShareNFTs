18e2dc8c27ddb50c7597478bf866008dcf08ded5
Phuc Nguyen <40540902+phucprime@users.noreply.github.com>
2022-01-14 22:13:15 +0700

    Update styles.js

--- App/Navigator/Drawer/styles.js
+++ App/Navigator/Drawer/styles.js
@@ -1,41 +1,41 @@
-import {Dimensions} from 'react-native';
-import {Colors, FontSize} from 'Theme';
-const {height} = Dimensions.get('window');
+import { Dimensions } from "react-native";
+import { Colors, FontSize } from "Theme";
+const { height } = Dimensions.get("window");
 
 const styles = {
   drawer: {
-    rootViewContainerStyle: {flexGrow: 1},
+    rootViewContainerStyle: { flexGrow: 1 },
     rootViewStyle: {
       height: height * 0.225,
-      width: '100%',
-      justifyContent: 'flex-end',
+      width: "100%",
+      justifyContent: "flex-end",
       backgroundColor: Colors.primary,
     },
     imageContainerStyle: {
-      height: '80%',
-      width: '45%',
-      alignItems: 'center',
-      justifyContent: 'space-evenly',
+      height: "80%",
+      width: "45%",
+      alignItems: "center",
+      justifyContent: "space-evenly",
     },
     moreContentContainerStyle: {
       marginVertical: 10,
       height: 1,
-      backgroundColor: '#7777',
+      backgroundColor: "#7777",
     },
   },
   screen: {
     rootViewStyle: {
       height: height * 0.07,
       marginBottom: 1,
-      width: '100%',
-      flexDirection: 'row',
-      alignItems: 'center',
-      paddingHorizontal: '5%',
+      width: "100%",
+      flexDirection: "row",
+      alignItems: "center",
+      paddingHorizontal: "5%",
     },
     textStyle: {
       fontSize: FontSize.body - 2,
       color: Colors.text,
-      marginLeft: '7.5%',
+      marginLeft: "7.5%",
     },
   },
 };
